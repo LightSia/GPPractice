9263c791d717615efd2962050afc2091d090b634
namkyu <knk1002@kaist.ac.kr>
2020-06-08 01:52:35 +0900

    Nguyen-8 마무리

--- Nguyen-7.py
+++ Nguyen-7.py
@@ -12,15 +12,27 @@ from deap import creator
 from deap import tools
 from deap import gp
 
-import pygraphviz as pgv
 
+def protectedDiv(left, right):
+    try:
+        return left / right
+    except ZeroDivisionError:
+        return 1
+
+def protectedLog(input) :
+    if(input <= 0):
+        return 0
+    
+    return math.log(input)
 
 pset = gp.PrimitiveSet("MAIN", 1)
 pset.addPrimitive(operator.add, 2)
+pset.addPrimitive(operator.sub, 2)
 pset.addPrimitive(operator.mul, 2)
-pset.addPrimitive(operator.neg, 1)
-pset.addPrimitive(math.log, 1)
+pset.addPrimitive(protectedDiv, 2)
+pset.addPrimitive(protectedLog, 1)
 pset.addTerminal(1)
+
 pset.renameArguments(ARG0='x')
 
 creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
@@ -29,6 +41,32 @@ creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
 toolbox = base.Toolbox()
 toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=5)
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+toolbox.register("compile", gp.compile, pset=pset)
 
-expr = toolbox.individual()
-nodes, edges, labels = gp.graph(expr)
+def evalSymbReg(individual, points):
+    func = toolbox.compile(expr=individual)
+    sqerrors = ((func(x) - (protectedLog(x + 1) + protectedLog(x*x + 1)))**2 for x in points)
+    return math.fsum(sqerrors) / len(points),
+
+toolbox.register("evaluate", evalSymbReg, points=[x/10 for x in range(0,20)])
+toolbox.register("select", tools.selTournament, tournsize=20)
+toolbox.register("mate", gp.cxOnePoint)
+toolbox.register("expr_mut", gp.genFull, min_=1, max_=5)
+toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+
+toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
+toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
+
+pop = toolbox.population(n=500)
+hof = tools.HallOfFame(1)
+stats_fit = tools.Statistics(lambda ind: ind.fitness.values)
+stats_size = tools.Statistics(len)
+mstats = tools.MultiStatistics(fitness=stats_fit, size=stats_size)
+mstats.register("avg", numpy.mean)
+mstats.register("std", numpy.std)
+mstats.register("min", numpy.min)
+mstats.register("max", numpy.max)
+
+pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.1, 40, stats=mstats,
+                                halloffame=hof, verbose=True)
